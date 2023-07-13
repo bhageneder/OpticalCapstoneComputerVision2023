9153d55d0b0b70187a996f0629b97e9b24967c99
Mitchell-Conrad-20 <Mitchell.Conrad@gmail.com>
2023-07-12 22:03:50 -0400

    Better windows, less overhead

--- Computer-Vision/test.py
+++ Computer-Vision/test.py
@@ -1,4 +1,4 @@
-from Detection import Detection
+from Detection_mini import Detection
 from threading import Thread
 
 self.thread = Thread(target = Detection(1280, 360, "Balls_2", [0,1], True), args = (), daemon=True, name="Detect")
