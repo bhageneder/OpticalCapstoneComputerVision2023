d5ff5ae80e905378da953c5f9a0f7410a2572c1a
Mitchell Conrad <Mitchell.conrad@gmail.com>
2023-07-27 17:02:12 -0400

    Update sendTranscieverUART.py

--- Computer-Vision/sendTranscieverUART.py
+++ Computer-Vision/sendTranscieverUART.py
@@ -3,36 +3,39 @@ from threading import Thread
 from UART import UART 
 
 ##### Sends the Transceiver Assignment over UART to the Pi #####
+def main():
+    # Only use the cameras in this set
+    cameras = [0]
 
-# Only use the cameras in this set
-cameras = [0]
+    # Initialize Detection
+    detection = Detection(1280, 720, "robots_only", cameras, True, False)
 
-# Initialize Detection
-detection = Detection(1280, 720, "robots_only", cameras, True, False)
+    # Run Detection in a Thread
+    thread = Thread(target = detection.detect, args = (), daemon=True, name="Detect")
+    thread.start()
 
-# Run Detection in a Thread
-thread = Thread(target = detection.detect, args = (), daemon=True, name="Detect")
-thread.start()
+    # Wait Until Detection is Initialized
+    while True:
+        if(not detection.initializing):
+            break
 
-# Wait Until Detection is Initialized
-while True:
-    if(not detection.initializing):
-        break
+    # Initialize UART
+    connection = UART()
 
-# Initialize UART
-connection = UART()
+    # Infinite Loop to Send Data
+    while(True):       
+        try:	
+            transceiver = detection.getTransceiver()
+            connection.send(transceiver)
+        except KeyboardInterrupt:
+            break
+        except:
+            print("Detection Error") 
 
-# Infinite Loop to Send Data
-while(True):       
-    try:	
-        transceiver = detection.getTransceiver()
-        connection.send(transceiver)
-    except KeyboardInterrupt:
-        break
-    except:
-        print("Detection Error") 
+    # Close Serial Connection
+    connection.close()
 
-# Close Serial Connection
-connection.close()
+    exit(0)
 
-exit(0)
+if __name__ == "__main__":
+    main()
