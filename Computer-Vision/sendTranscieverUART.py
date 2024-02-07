511e14c1c209804d50b75133bf38e74c8a6e6f62
bhageneder <bhageneder@ycp.edu>
2024-02-07 10:13:23 -0500

    Removed MIPI camera due to nvargus-daemon crashing

--- Computer-Vision/sendTranscieverUART.py
+++ Computer-Vision/sendTranscieverUART.py
@@ -9,9 +9,10 @@ def main():
     camSet1 = 'nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=1280, height=720, format=(string)NV12, framerate=30/1 ! nvvidconv flip-method="2" ! video/x-raw, width=1280, height=720, format=(string)BGRx ! videoconvert ! appsink'
 
     #cameras = [camSet1, 2, 1]
+    cameras = [2,1]
 
-    # Initialize Detection    
-    detector = Detector(1280, 240, "Robot_Model_Pan2", cameras, True, True, True)
+    # Initialize Detection    240
+    detector = Detector(1280, 360, "Robot_Model_Pan2", cameras, True, True, True)
 
     # Run Detection in a Thread
     thread = Thread(target = detector.detect, args = (), daemon=True, name="Detect")
