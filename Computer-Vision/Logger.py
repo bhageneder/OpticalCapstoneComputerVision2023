3133c95b659cd4ad7ec44ca7826719f0e20ab857
damao21 <damao@ycp.edu>
2024-02-26 16:07:52 -0500

    test

--- Computer-Vision/Logger.py
+++ Computer-Vision/Logger.py
@@ -1,72 +1,155 @@
+from tkinter import NONE
 from loguru import logger
 import sqlite3
 from datetime import datetime
 import os
+import csv
 
 
 class Logger:
-    DEFAULT_LOG_FILE_PATH = 'logger.db'
-    def __init__(self, log_file_path = None):
-        self.log_file_path = self.DEFAULT_LOG_FILE_PATH
+    DEFAULT_LOG_FILE = 'logger.db'
+    DEFAULT_CSV_FOLDER = 'csvFolder.csv'
+
+    levels = {
+        logger.DEBUG: 'Debug',
+        logger.INFO: 'Info',
+        logger.WARNING: 'Warning',
+        logger.ERROR: 'Error',
+        logger.CRITICAL: 'Critical'
+        }
+
+
+    def __init__(self, logFilepath = None):
+        self.logFilepath = self.DEFAULT_LOG_FILE
+        self.id = id
+        self.tag = tag
+        self.type = type
+        self.module = module
+        self.message = message
+
+        self.LEVEL_DEBUG = 0
 
         
-        # Check if the database file exists, if not, create it
-        if not os.path.exists(self.log_file_path):
-            if self.create_database():
-                print("Database Created Successfully: {self.log_file_path}")
-            else:
-                print("Database Creation Failed: {self.log_file_path}")
+        # Obtain sql database connection
+        self.sqliteConnection = self.createDatabaseConnection(self)
 
-    # Set loguru to use SQLite sink
-        logger.add(self.log_file_path, table='log_table', 
-                   format="{time:YYYY-MM-DD HH:mm:ss} | {level: <8} | <ID: {record[id]}> | "
-                        "<Timestamp: {record[timestamp]}> | <Process ID: {record[process_id]}> | " 
-                        "<Module: {record[module_name]}> | <Category: {record[category]}> | " 
-                        "<Message: {record[message]}>", 
-                    level='INFO', enqueue=True, serialize=True)
-        
-        # Create / open database existing on disk
-        self.sqliteConnection = sqlite3.connect(self.log_file_path)
         # Add cursor to retrieve data from database using queries
         self.cursor = self.sqliteConnection.cursor()
-        
-        # Define the table
-        log_table = ''' Create Table event_Log(
+
+        # Define the event table
+        self.eventTable =  """ CREATE TABLE eventLog(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-            time TEXT,
-            level TEXT,
-            process_id INTEGER,
-            module_name TEXT,
-            category TEXT,
-            message TEXT
-        )'''
+            Timestamp TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+            ProcessID INTEGER,
+            Tag VARCHAR(10) None,
+            Type INTEGER,
+            Module  VARCHAR(30),
+            Message VARCHAR(255),
+        )"""
+
+        self.levelTable = """ CREATE TABLE levelLog(
+            severance VARCHAR(20), 
+            Message VARCHAR(100)
+        )"""
+
+    # Logger Class Deconstructor
+    def __del__(self):
+        print("Commiting changes to Database and Deconstructing Logger")
         
+        # Close sql connection 
+        self.sqliteConnection.close()
+
+    # Create / open database existing on disk and return database connection
+    def createDatabaseConnection(self):
+        try:
+            # Check if connection exists 
+            conn = sqlite3.connect(self.logFilepath)
+            return conn
+        except sqlite3.Error as sqliteError:
+            print("Error connecting to SQLite database at path: {}".format(self.logFilePath))
+            print(sqliteError)
+            return NONE
+        except Exception as e:
+            print("FATAL ERROR connecting to database: {e}")
+            return NONE
+      
+    def logSetup(self):
         # Get the process ID and module name
         pid = os.getpid()
-        # Used to store the full path of the script file in the log_table as a way to identify which script or module inserted a particular log entry
+        # Used to store the full path of the script file in the eventTable as a way to identify which script or module inserted a particular log entry
         modname = __file__ 
 
-        # Execute the table 
+        # Export logs to CSV when an instance is created
+        csvFilepath = self.export_to_csv()
+        logger.INFO('Logs exported to CSV: {csvFilepath}')
+
+        # Execute the tables
         # Use cursor execute to populate all desired data into table
         print("Collected Data: ") # --> not really necessary
-        # To select specific column replace * with the column name(s) ('''SELECT * FROM log_table''')
-        events = self.cursor.execute(log_table)
-       
-    
-    def create_database(self):
-        try:
-            # Create an empty database file if it doesn't exist
-            with open(self.log_file_path, 'w'):
-                pass
-            return True
-        except Exception as e:
-            print("Error creating database: {e}")
-            return False
-
-
-    # Commit Changes to Database
-    def close_connection(self):        
+        # To select specific column replace * with the column name(s) ('''SELECT * FROM eventTable''')
+        event = self.cursor.execute(self.eventTable)
+        
+        # Set loguru to use SQLite sink
+        logger.add(self.logFilepath, table='eventTable', 
+                   format=" <ID: {record[id]}> | <Timestamp: {record[timestamp]}> | <Process ID: {record[processID]}> | "
+                        "<Tag:{record[tag]}> | <Type:{record[type]}> | <Module: {record[module_name]}> |" 
+                        " |<Category: {record[category]}> ", 
+                    level='INFO', enqueue=True, serialize=True) #level may be unecessary
+        # Commit Changes to Database
         self.sqliteConnection.commit()
-    # Close SQL connection
-        self.sqliteConnection.close()
+
+    def addEvent(self,level,type,module,message):
+    
+        '''types = {
+            logger.Detector: 1;
+            logger.image: 2; #360image
+            logger.multiOD: 3;
+            logger.multiCamV: 4;
+            logger.sendTrans: 5;
+        }''' 
+       # instantiantion method detection = logging.getLogger(Detect)
+        
+
+        if level in levels:
+            # Execute SQL statements based on the log level
+            self.cursor.execute("INSERT INTO eventTable (id, timestamp, Type, Module) VALUES (?, ?, ?, ?)", (id, timestamp, type, module))
+            self.cursor.execute("INSERT INTO levelTable (severance, Message) VALUES (?, ?)", (levels[level], message))
+            getattr(logger, levels[level])("{levels[level]}: {message}")
+        
+        else:
+            print("Unknown log level: {level}")
+
+        
+        self.sqliteConnection.commit()
+
+
+ 
+
+
+    def exportCsv(self, csvFilepath):
+        try:
+            # Retrieve all logs from the database
+            self.cursor.execute("SELECT * FROM eventTable")
+            self.sqliteConnection.commit()
+            rows = self.cursor.fetchall()
+            
+            # Create a folder for storing CSV files if it doesn't exist
+            csvFolder = os.path.join(os.path.dirname(self.logFilepath), self.DEFAULT_CSV_FOLDER)
+            os.makedirs(csvFolder, exist_ok=True)
+
+            # Write logs to a CSV file
+            with open(csvFilepath, 'w', newline='') as csvFile:
+                csvWriter = csv.writer(csvFile)
+                header = ["ID", "Timestamp", "Process ID", "Tag", "Type", "Module", "Category", "Message"]
+                csvWriter.writerow(header)
+                csvWriter.writerows(rows) #need to create separate csv for data table 
+        
+            # Update the database with the CSV file path
+            self.cursor.execute("UPDATE eventTable SET csvFilepath = ?", (csvFilepath))
+            self.sqliteConnection.commit()
+            print("Logs exported to CSV: {csvFilepath}")
+
+            return csvFilepath
+
+        except Exception as e:
+            print("Error exporting logs to CSV: {e}")
