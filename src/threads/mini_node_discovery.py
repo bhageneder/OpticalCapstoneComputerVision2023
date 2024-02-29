ca38b49cde9482d9692e65f19d1a3d96057d5799
Mitchell Conrad <Mitchell.conrad@gmail.com>
2024-02-29 12:34:30 -0500

    Update mini_node_discovery.py

--- src/threads/mini_node_discovery.py
+++ src/threads/mini_node_discovery.py
@@ -29,39 +29,18 @@ def mini_discovery(robot_receiving_ip_address, dst_port, client_port, robot):
             # Send SYN packets using send manager
             client_socket.connect((robot_receiving_ip_address, int(dst_port)))
 
-            ''' ### NeoPixel Ring, Needs to be Updated. Use robot.transceiver
-            # if no robot found, make all transceivers red
-            if best_transceiver_number == -1:
-                #for transceiver in g.best_transceiver:
-                lc.illuminate_for_finding(best_transceiver_number)
-            # if robot found, make that transceiver blue
-            else:
-                lc.illuminate_for_connecting(best_transceiver_number)
-            '''
     except socket.timeout:
-        ''' ### NeoPixel continued
-        #if g.debug_mini_discovery: print(f'{thread_name} Socket Timeout')
-        #for transceiver in g.best_transceiver:
-        lc.turn_off_for_finding(g.best_transceiver)
-        lc.turn_off_for_connecting(g.best_transceiver)
-        '''
         client_socket.close() # Close the socket to unbind it
         return
     
     if g.debug_mini_discovery: print(f'{thread_name} Connected')
-    
+
     for robot in g.visible:
             if robot.robotLink.ip_address == robot_receiving_ip_address:
                 break
     else:
         # To make the socket never timed out now when sending or receiving data
         client_socket.settimeout(None) 
-
-        ''' ### NeoPixel
-        #for transceiver in g.best_transceiver:
-        lc.turn_off_for_finding(g.best_transceiver)
-        lc.turn_off_for_connecting(g.best_transceiver)
-        '''
 
         # Default the serial port to transceiver 0, Maintenance will set the best one.
         link = RobotLink(None, g.serial_ports[0], client_socket, robot_receiving_ip_address, dst_port)
