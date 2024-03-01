638c125443eeb86c97b2ded578f3d88fc04cd7aa
Mitchell Conrad <Mitchell.conrad@gmail.com>
2024-03-01 14:44:44 -0500

    Update link_send.py

--- src/threads/link_send.py
+++ src/threads/link_send.py
@@ -42,15 +42,14 @@ def link_send_legacy(robot_link):
     payload = b'hello'
     send_num = 0
 
-    if g.LEGACY_MODE:
-        while True:
-            # Send Payload through Socket 
-            if g.debug_link_send: print(f'{thread_name} Sending Payload through TCP Socket {payload}{send_num}')
-            robot_link.socket.sendall(payload + send_num.to_bytes(4, byteorder="little"))
+    while True:
+        # Send Payload through Socket 
+        if g.debug_link_send: print(f'{thread_name} Sending Payload through TCP Socket {payload}{send_num}')
+        robot_link.socket.sendall(payload + send_num.to_bytes(4, byteorder="little"))
 
-            send_num += 1
-            time.sleep(g.PAYLOAD_INTERVAL_SLEEP)
+        send_num += 1
+        time.sleep(g.PAYLOAD_INTERVAL_SLEEP)
 
-            # Terminate if robot_link no longer exists
-            if robot_link not in g.robot_links:
-                return
+        # Terminate if robot_link no longer exists
+        if robot_link not in g.robot_links:
+            return
