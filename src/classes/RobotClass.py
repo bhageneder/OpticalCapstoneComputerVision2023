29e56bcf49e10f8cf72cfade53d510a7f656672c
bhagened <bhageneder@ycp.edu>
2024-03-19 20:54:23 -0400

    Update RobotClass as per comments

--- src/classes/RobotClass.py
+++ src/classes/RobotClass.py
@@ -1,4 +1,4 @@
-class Robot():
+class Robot:
     def __init__(self, trackingID, transceiver, losActive, RobotLink = None):
         self.trackingID = trackingID        # Integer - ID number associated with the robot in the detection loop
         self.transceiver = transceiver      # Integer - Best transceiver to use for communications
