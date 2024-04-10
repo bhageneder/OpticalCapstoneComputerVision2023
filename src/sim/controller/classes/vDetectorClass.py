21a94e3515057e1028a6bca8e8b9e31027caa9bf
bhagened <bhageneder@ycp.edu>
2024-04-10 01:13:09 -0400

    Update vDetectorClass.py
    Updated detect() logic for vDetector and comments.

--- src/sim/controller/classes/vDetectorClass.py
+++ src/sim/controller/classes/vDetectorClass.py
@@ -6,83 +6,114 @@ from interfaces.BaseDetectorClass import BaseDetector
 class vDetector(BaseDetector):
     def __init__(self, robotModel, model):
         super.__init__
-        self.__robotModel = robotModel
-        self.__model = model
+        self.__robotModel = robotModel # Current Robot Model
+        self.__model = model # Complete MVC Model
         self.__threshold = 300  # Arbitrary threshold of 300 pixels
 
 
     def __distance_between_points(self, x1, y1, x2, y2):
+        # Return the distance between points
         return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
     
     def __angle(self, x1, y1, x2, y2):
         # Calculate the angle in radians using arctan2
         ang_rad = math.atan2(x2 - x1, y2 - y1)
+
         # Convert radians to degrees
         ang_deg = math.degrees(ang_rad)
+
         # Ensure the angle is between 0 and 360 degrees
         ang_deg %= 360
+
         return ang_deg
 
     def __blocking(self, currentRobot, targetRobot, blocker):
-        # Create a LineString representing the line segment between obj1 and obj2
+        # Create a LineString representing the line segment between robots
         line = LineString([currentRobot, targetRobot])
 
-        # Convert obj3's shape to a Shapely geometry object
-        blockerGeometry = blocker
-
-        # Check if the line intersects with obj3's shape
-        return line.intersects(blockerGeometry)
+        # Check if the line intersects with blocker
+        return line.intersects(blocker)
 
 
     def detect(self):
+        # Start detection loop
         while True:
+            # Stop detection loop if robot is deleted
+            if not (self.__robotModel.robotItem.isActive()):
+                break
+            
+            # iterate through all known robots
             for robot in self.__model.robots:
+                
+                # Don't care about current robot
                 if (robot.ip == self.__robotModel.ip):
                     pass
                 else:
-                    if not (self.__robotModel.robotItem.isActive()):
-                        break
-                    blocking = False
-                    for blockerObj in self.__model.blockers:
-                        blockerItem = blockerObj.blockerItem
-                        boundingRect = blockerItem.boundingRect()
-                        # Get the positions of the four corners
-                        TL = blockerItem.mapToScene(boundingRect.topLeft())
-                        TR = blockerItem.mapToScene(boundingRect.topRight())
-                        BL = blockerItem.mapToScene(boundingRect.bottomLeft()) 
-                        BR = blockerItem.mapToScene(boundingRect.bottomRight())
-                        # Define the rectangle as a Polygon (bottom-left, bottom-right, top-right, top-left)
-                        blocker = Polygon([(BL.x(), BL.y()), (BR.x(), BR.y()), (TR.x(), TR.y()), (TL.x(), TL.y())])
 
-                        blocking = self.__blocking(currentRobot, targetRobot, blocker)
-                        if blocking:
-                            break
-                    if blocking:
-                        if robot.ip in self.__robotModel.detections:
-                            self.__robotModel.detections.remove(robot.ip)
-                    else:
-                        currentRobot = (self.__robotModel.robotItem.pos().x(), self.__robotModel.robotItem.pos().y())
-                        targetRobot = (robot.robotItem.pos().x(), robot.robotItem.pos().y())
-                        distance = self.__distance_between_points(
-                                                                currentRobot[0],
-                                                                currentRobot[1],
-                                                                targetRobot[0],
-                                                                targetRobot[1]
-                                                                )
-                        angle = self.__angle(
+                    # current robot's x,y position
+                    currentRobotPos = self.__robotModel.robotItem.mapToScene(self.__robotModel.robotItem.boundingRect().center())
+                    currentRobot = (currentRobotPos.x(),
+                                    currentRobotPos.y())
+
+                    # target robots x,y position
+                    targetRobotPos = robot.robotItem.mapToScene(self.__robotModel.robotItem.boundingRect().center())
+                    targetRobot = (targetRobotPos.x(),
+                                   targetRobotPos.y())
+                    
+                    # obtain the distance between two robots
+                    distance = self.__distance_between_points(
+                                                            currentRobot[0],
+                                                            currentRobot[1],
+                                                            targetRobot[0],
+                                                            targetRobot[1]
+                                                            )
+                    
+                    # obtain the angle between two robots
+                    angle = self.__angle(
                                         currentRobot[0],
                                         currentRobot[1],
                                         targetRobot[0],
                                         targetRobot[1]
-                        )
-                        if (distance <= self.__threshold):
-                            if robot.ip not in self.__robotModel.detections:
-                                self.__robotModel.detections.append(robot.ip)
-                            #print("INFO:    {} Detected {}".format(self.__robotModel.ip, robot.ip)) # Helpful print statement
+                                        )
+
+                    if (distance <= self.__threshold):
+                        # if distance is within threshold; if blockers, not detectect : otherwise, detected
+                        blocking = False
+                        
+                        # iterate through all known blockers
+                        for blockerObj in self.__model.blockers:
+
+                            blockerItem = blockerObj.blockerItem
+                            blockerRect = blockerItem.rect()
+
+                            # Get updated positions of the four blocker corners
+                            TL = blockerItem.mapToScene(blockerRect.topLeft())
+                            TR = blockerItem.mapToScene(blockerRect.topRight())
+                            BL = blockerItem.mapToScene(blockerRect.bottomLeft()) 
+                            BR = blockerItem.mapToScene(blockerRect.bottomRight())
+
+                            # Define the rectangle as a Polygon (bottom-left, bottom-right, top-right, top-left)
+                            blocker = Polygon([(BL.x(), BL.y()), (BR.x(), BR.y()), (TR.x(), TR.y()), (TL.x(), TL.y())])
+
+                            blocking = self.__blocking(currentRobot, targetRobot, blocker)
+
+                            if blocking:
+                                # blocker found, DNC if more than one blocker between two robots, end search
+                                break
+
+                        if blocking:
+                            # Remove robot from detections list when blocked
+                            self.__robotModel.detections.remove(robot.ip) if robot.ip in self.__robotModel.detections else None                                
                         else:
-                            if robot.ip in self.__robotModel.detections:
-                                self.__robotModel.detections.remove(robot.ip)
+                            # Add robot to detections list of not blocked
+                            self.__robotModel.detections.append(robot.ip) if robot.ip not in self.__robotModel.detections else None
+                                
+                        #print("INFO:    {} Detected {}".format(self.__robotModel.ip, robot.ip)) # Helpful print statement
+                    else:
+                        self.__robotModel.detections.remove(robot.ip) if robot.ip in self.__robotModel.detections else None
+                
                 if (self.__robotModel.ip == robot.ip):
+                    # Helpful print statement
                     print("List of Detected Robots for {}:  {}".format(self.__robotModel.ip, self.__robotModel.detections))
 
             time.sleep(1)
