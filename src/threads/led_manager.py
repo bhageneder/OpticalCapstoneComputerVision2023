9875d2951fcb50e364f04ff3f8ae285ed6cab6a3
stoneoxmike <15141319+stoneoxmike@users.noreply.github.com>
2024-04-17 15:54:37 -0400

    Legacy mode LEDs

--- src/threads/led_manager.py
+++ src/threads/led_manager.py
@@ -15,30 +15,32 @@ def led_manager():
         #background process to loop through LED arrays and set LEDs
         LEDs = g.LEDs.getLEDs()
 
-        for robot in g.lost:
-            if robot.transceiver > -1:
-                g.LEDs.on("lost", robot.transceiver)
+        if g.LEGACY_MODE:
+            for link in g.robot_links:
+                g.LEDs.on("connected", link.port)
+        else:
+            for robot in g.lost:
+                if transceiver > -1:
+                    g.LEDs.on("lost", robot.transceiver)
 
-        for robot in g.visible:
-            if robot.robotLink is None:
-                g.LEDs.on("finding", robot.transceiver)
-            else:
-                g.LEDs.on("connected", robot.transceiver)
+            for robot in g.visible:
+                if robot.robotLink is None:
+                    g.LEDs.on("finding", robot.transceiver)
+                else:
+                    g.LEDs.on("connected", robot.transceiver)
 
         for transceiver in range(8):
-            if LEDs[0][transceiver] > 0:
+            if LEDs[0][transceiver] == 1:
                 led_funcs.illuminate_for_finding(transceiver)
             else:
                 led_funcs.turn_off_for_finding(transceiver)
-            if LEDs[1][transceiver] > 0:
+            if LEDs[1][transceiver] == 1:
                 led_funcs.illuminate_for_connected(transceiver)
             else:
                 led_funcs.turn_off_for_connected(transceiver)
-            if LEDs[2][transceiver] > 0:
+            if LEDs[2][transceiver] == 1:
                 led_funcs.illuminate_for_lost(transceiver)
             else:
                 led_funcs.turn_off_for_lost(transceiver)
-
-        g.LEDs.reset()
 
         time.sleep(0.25)
