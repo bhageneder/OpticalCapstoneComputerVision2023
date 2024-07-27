667c29bf41988854ba530b88bc0e5ab8e647a25b
Mitchell Conrad <Mitchell.conrad@gmail.com>
2024-07-27 14:47:56 -0400

    Setup routing

---
+++ src/sim/controller/classes/vRouter.py
@@ -0,0 +1,28 @@
+
+#### Router Class ####
+
+# Routing Table (Very Rudementary) Format:
+# dictionary = {
+#     "10.0.0.10": [("10.0.0.11", 1), ("10.0.0.12", 2)],
+#     "sourceIP": [("destIP", numHops), ...]
+# }
+
+class vRouter():
+    def __init__(self):
+        self.__table = dict()
+
+    # Efficiently update the key-value pair
+    def updateRoute(self, sourceIP, routeArray):
+        self.__table.update({sourceIP, routeArray})
+
+    # Inefficiently search the table for the best route
+    def findRoute(self, destIP):
+        bestRoute = (None, 65535)
+        
+        for key in self.__table.keys():
+            possibleRoutes = self.__table.get(key)
+            for tuple in possibleRoutes:
+                if (tuple[0] == destIP and tuple[1] < bestRoute[1]):
+                    bestRoute = tuple
+
+        return bestRoute[0]
